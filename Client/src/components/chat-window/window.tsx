e2e19034ab6074f06e64757f64fa39d2edb75af5
Seapeas <dodinhhai29904@gmail.com>
2025-04-23 11:49:36 +0700

    feat: update .gitignore to include Python environment files and cache directories; add uuid dependency to package.json and package-lock.json; implement Chats page with notebook creation and editing functionality

--- Client/src/components/chat-window/window.tsx
+++ Client/src/components/chat-window/window.tsx
@@ -1,11 +1,13 @@
 import { Input } from "@/components/ui/input";
-import { useRef } from "react";
-import { LoremIpsum } from "react-lorem-ipsum";
-import { Skeleton } from "@/components/ui/skeleton";
+import { useRef, useState} from "react";
+// import { LoremIpsum } from "react-lorem-ipsum";
+// import { Skeleton } from "@/components/ui/skeleton";
 
 export default function ChatWindow() {
 	const inputRef = useRef<HTMLInputElement>(null);
 	const chatWindowRef = useRef<HTMLDivElement>(null);
+	const chatBodyRef = useRef<HTMLDivElement>(null);
+	const [messages, setMessages] = useState<string[]>([]);
 	return (
 		<>
 			<div
@@ -13,27 +15,48 @@ export default function ChatWindow() {
 				ref={chatWindowRef}
 			>
 				<div className="w-full h-7/8 bg-zinc-900 flex flex-col justify-center items-center">
-					<div className="w-full h-full bg-zinc-900 rounded-xl flex flex-col overflow-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none][scrollbar-width:none]">
-						<div className="w-fit max-w-1/2 h-fit text-white ml-auto bg-zinc-800 p-4 rounded-4xl">
+					<div
+						className="w-full h-full bg-zinc-900 rounded-xl flex flex-col overflow-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none][scrollbar-width:none]"
+						ref={chatBodyRef}
+					>
+						{/* <div className="w-fit max-w-1/2 h-fit text-white ml-auto bg-zinc-800 p-4 rounded-4xl">
 							<LoremIpsum p={3} />
-						</div>
-						<Skeleton className="h-96 m-8 box-border bg-red-500" />
+						</div> */}
+						{messages.map((message, index) => (
+							<div
+								key={index}
+								className="w-auto max-w-1/2 h-fit text-white ml-auto bg-zinc-800 p-4 rounded-4xl mb-2 text-pretty whitespace-normal break-words"
+							>
+								{message}
+							</div>
+						))}
+						{/* <Skeleton className="h-96 m-8 box-border bg-red-500" /> */}
 					</div>
 				</div>
 				<div className="w-7/8 h-1/8 bg-zinc-900 flex flex-col justify-center items-end">
 					<span className="text-white ml-4">100 sources</span>
 					<Input
+						type="text"
+						placeholder="Type your message here..."
 						className="text-white w-full"
 						ref={inputRef}
 						onKeyDown={(e) => {
 							if (e.key === "Enter" && inputRef.current) {
 								const inputValue = inputRef.current.value;
 								if (inputValue.trim() !== "") {
-									console.log("Input value:", inputValue);
+									setMessages((prevMessages) => [
+										...prevMessages,
+										inputValue,
+									]);
 									// Handle the input value here
 									inputRef.current.value = ""; // Clear the input after submission
 								}
 							}
+						}}
+						onPaste={(e) => {
+							const clipboardData = e.clipboardData;
+							const pastedData = clipboardData.getData("text");
+							inputRef.current!.value += pastedData;
 						}}
 					/>
 				</div>
--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App.tsx";
 import Register from "./pages/register/page.tsx";
 import Reset from "./pages/reset-password/page.tsx";
 import Chat from "./pages/chat/page.tsx";
+import Chats from "./pages/chats/page.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<BrowserRouter>
@@ -14,6 +15,8 @@ createRoot(document.getElementById("root")!).render(
 			<Route path="/register" element={<Register />} />
 			<Route path="/reset" element={<Reset />} />
 			<Route path="/chat" element={<Chat />} />
+			<Route path="/chat/:id" element={<Chat />} />
+			<Route path="/chats" element={<Chats />} />
 		</Routes>
 	</BrowserRouter>
 );
